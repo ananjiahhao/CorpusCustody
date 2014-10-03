1c401dfe76a3970c3d615f302fc9abf99fe54271
kinetic <20599331+ananjiahhao@users.noreply.github.com>
2014-10-02 18:21:00 -0700

    fix: spdx

--- src/corpuscustody/manifest.py
+++ src/corpuscustody/manifest.py
@@ -5,3 +5,7 @@ whose first non-space character is `#` are ignored, so manifests can carry
 comments and stay diffable.
 
 Each record line has three tab-separated or pipe-separated fields:
+
+    record_id  <sep>  spdx_id  <sep>  source
+
+The separator is a single pipe `|`. Surrounding whitespace on each field is
