e0aa427837560db528df67abdce3e357bc63a9c2
kinetic <20599331+ananjiahhao@users.noreply.github.com>
2015-01-09 09:21:00 -0700

    chore: bump python

--- src/corpuscustody/spdx.py
+++ src/corpuscustody/spdx.py
@@ -30,3 +30,10 @@ OBLIGATIONS: List[str] = [
 
 
 @dataclass(frozen=True)
+class License:
+    """A license identifier and the obligations it imposes."""
+
+    spdx_id: str
+    attribution: bool = False
+    share_alike: bool = False
+    non_commercial: bool = False
