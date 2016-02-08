bf63dd7f705811d16e9f127d7aad47591457a423
kinetic <20599331+ananjiahhao@users.noreply.github.com>
2016-02-08 10:11:00 -0700

    perf: improve python

--- src/corpuscustody/gate.py
+++ src/corpuscustody/gate.py
@@ -60,3 +60,12 @@ def decide(records: List[Record], purpose: str) -> GateResult:
 def cleared_manifest_lines(records: List[Record], purpose: str) -> List[str]:
     """Build the cleared manifest lines for a passing set.
 
+    Returns a header comment block followed by one cleared line per record.
+    Lines are returned without trailing newlines so the caller controls the
+    line terminator and byte-identical output stays under its control.
+    """
+    lines: List[str] = []
+    lines.append("# corpuscustody cleared manifest")
+    lines.append("# purpose: {0}".format(purpose))
+    lines.append("# record_id | spdx_id | cleared_for")
+    for record in records:
